99ee7a5aa275f69d2353959040d59dd449589026
João Avelino <joaolavelino@gmail.com>
2025-06-27 14:10:49 -0300

    styled components setup

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,27 @@
+export const defaultTheme = {
+  blue: "#320f48",
+  base: {
+    title: "#E7EDF4",
+    subtitle: "#C4D4E3",
+    text: "#AFC2D4",
+    span: "#7B96B2",
+    label: "#3A536B",
+    border: "#1C2F41",
+    post: "#112131",
+    profile: "#0B1B2B",
+    bg: "#071422",
+    input: "#040F1A",
+  },
+  font: {
+    title: `600 "Jost", sans-serif;`,
+    text: `300 "Figtree", sans-serif;`,
+  },
+  mediaQuery: {
+    xsm: `@media screen and (max-width: 400px)`, // Mobile landscape
+    sm: `@media screen and (min-width: 640px)`, // Mobile landscape
+    md: `@media screen and (min-width: 768px)`, // Tablets
+    lg: `@media screen and (min-width: 1024px)`, // Laptops
+    xl: `@media screen and (min-width: 1280px)`, // Desktops
+    xxl: `@media screen and (min-width: 1536px)`, // Large screens
+  },
+};
